571b9def9fedb6cefd8e8b8788e1a9c09b24723e
trananquan <130271615+trananquan@users.noreply.github.com>
2023-04-21 22:04:08 +0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,59 @@
+const cursor = document.querySelector('.cursor')
+const holes = [...document.querySelectorAll('.hole')]
+const scoreEl = document.querySelector('.score span')
+let score = 0
+
+
+
+const sound = new Audio("assets/smash.mp3")
+
+
+function run(){
+    const i = Math.floor(Math.random() * holes.length)
+    const hole = holes[i]
+    let timer = null
+
+    const img = document.createElement('img')
+    img.classList.add('mole')
+    img.src = 'assets/mole.png'
+    
+
+    img.addEventListener('click', () => {
+        score += 10
+        sound.play()
+        scoreEl.textContent = score
+        img.src = 'assets/mole-whacked.png'
+        clearTimeout(timer)
+        setTimeout(() => {
+            hole.removeChild(img)
+          run()
+        }, 500)
+    })
+
+    hole.appendChild(img)
+
+    timer = setTimeout(() => {
+        hole.removeChild(img)
+        run()
+    }, 1000)
+}
+run()
+
+let timeout;
+
+setTimeout ( function ( ) { alert( "Hết giờ! Click Ok để chơi lại !" ); window.location.href = "start.html"; }, 10000 );
+
+window.addEventListener('mousemove', e => {
+    cursor.style.top = e.pageY + 'px'
+    cursor.style.left = e.pageX + 'px'
+})
+window.addEventListener('mousedown', () => {
+    cursor.classList.add('active')
+})
+window.addEventListener('mouseup', () => {
+    cursor.classList.remove('active')
+})
+
+
+
+
